91aa56922914a681d0260a35a143d6bdfdbcbc61
Gustavo Monteiro <gustavosmonteiro@outlook.com>
2020-11-22 17:39:51 -0300

    remove transition

--- deputado/src/main/java/br/com/gustavomonteiro/deputado/ui/DeputadoDetailFragment.kt
+++ deputado/src/main/java/br/com/gustavomonteiro/deputado/ui/DeputadoDetailFragment.kt
@@ -3,7 +3,6 @@ package br.com.gustavomonteiro.deputado.ui
 import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.Fragment
-import androidx.transition.ChangeBounds
 import br.com.gustavomonteiro.core.viewBinding
 import br.com.gustavomonteiro.deputado.R
 import br.com.gustavomonteiro.deputado.databinding.DeputadoDetailFragmentBinding
@@ -12,19 +11,12 @@ import com.bumptech.glide.Glide
 class DeputadoDetailFragment : Fragment(R.layout.deputado_detail_fragment) {
     private val binding by viewBinding(DeputadoDetailFragmentBinding::bind)
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        sharedElementEnterTransition = ChangeBounds()
-    }
-
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        postponeEnterTransition()
 
         arguments?.getString(URL_PHOTO_KEY)?.let {
             Glide.with(this).load(it).into(binding.deputadoPhoto)
         }
-        startPostponedEnterTransition()
     }
 
     companion object {
@@ -40,5 +32,4 @@ class DeputadoDetailFragment : Fragment(R.layout.deputado_detail_fragment) {
                     }
             }
     }
-
 }
